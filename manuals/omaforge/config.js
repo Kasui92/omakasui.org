c91e7eb18b7e011d0ac80582b6c876bf4357f23f
Luca Pattocchio <pattocchioluca@gmail.com>
2025-08-29 00:14:42 +0200

    Added Omaforge manual

---
+++ manuals/omaforge/config.js
@@ -0,0 +1,30 @@
+window.DOCS_CONFIG = {
+  pages: {
+    home: {
+      file: "docs/home.html",
+      title: "Home",
+    },
+    installation: {
+      file: "docs/installation.html",
+      title: "Installation",
+    },
+    features: {
+      file: "docs/features.html",
+      title: "Features",
+    },
+    applications: {
+      file: "docs/applications.html",
+      title: "Applications",
+    },
+    hotkeys: {
+      file: "docs/hotkeys.html",
+      title: "Hotkeys",
+    },
+    "shell-functions": {
+      file: "docs/shell-functions.html",
+      title: "Shell Functions",
+    },
+  },
+  defaultPage: "home",
+  siteTitle: "The Omaforge Manual",
+};
