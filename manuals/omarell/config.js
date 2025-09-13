55e485f605c300ceb13cea6dd9f96e44e38f34ae
Luca Pattocchio <pattocchioluca@gmail.com>
2025-09-13 20:38:17 +0200

    Archive Omarell project

--- manuals/omarell/config.js
+++ manuals/omarell/config.js
@@ -4,22 +4,6 @@ window.DOCS_CONFIG = {
       file: "docs/home.html",
       title: "Home",
     },
-    installation: {
-      file: "docs/installation.html",
-      title: "Installation",
-    },
-    features: {
-      file: "docs/features.html",
-      title: "Features",
-    },
-    hotkeys: {
-      file: "docs/hotkeys.html",
-      title: "Hotkeys",
-    },
-    "shell-functions": {
-      file: "docs/shell-functions.html",
-      title: "Shell Functions",
-    },
   },
   defaultPage: "home",
   siteTitle: "The Omarell Manual",
